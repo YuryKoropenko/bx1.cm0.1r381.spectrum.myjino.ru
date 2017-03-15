a2c880529b9fd02c8b72a7308ee7603ab51ac4f7
Yury <yurykoropenko@gmail.com>
2017-03-15 17:02:51 +0200

    commit 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	/**/
+	$('.r-panel-item__element').on('click', function() {
+		$('.r-panel-item__element').removeClass('r-panel-item__element-active');
+		$(this).addClass('r-panel-item__element-active');
+	});
+});
